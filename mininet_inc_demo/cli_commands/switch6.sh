719329f5ebca031bcb563413943fdffec5058c1c
Faris <faris@null.net>
2025-10-15 18:07:13 +0200

    bugfix

--- mininet_inc_demo/cli_commands/switch6.sh
+++ mininet_inc_demo/cli_commands/switch6.sh
@@ -1,37 +1,13 @@
 # This is the first switch in the SDN domain,
 # we attach the input physical interface to it to process incoming packets
-port_add em0 100
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 192.168.60.40/32 => 2
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 192.168.70.40/32 => 2
 
-# Here we write the port numbers in the registers in order to
-# Associate the labels to output ports through the table MyIngress.tb_sdn_int_handler
-#                   Register Name         index  Output Port
-register_write MyIngress.rg_output_port     1        100
-register_write MyIngress.rg_output_port     2         2
-
-# This Table is used to insert SDN label (if does not exist) based on teh ipv4 destination address
-#                   Table Name                     Action Name              dest IPv4 LPM   Assigned label
-table_add   MyIngress.tb_add_sdn_int_from_ipv4    MyIngress.ac_sdn_int_push   10.30.7.213/32 =>   100
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 2 192.168.60.10/32 => 1
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 2 192.168.70.10/32 => 1
 
 
-# This Table is used to insert SDN label (if does not exist) based on the nextNF field of d6gmain header
-#                   Table Name                              Action Name              nextNF   Assigned label
-table_add   MyIngress.tb_add_sdn_int_from_d6gmain   MyIngress.ac_sdn_int_push          10      =>  100
-
-
-# This table handles the current packet using the SDN label, ac_sdn_int_handle assigs it a register index to read from it the output port
-#               Table Name                      Action Name              SDN Label    Register index
-table_add   MyIngress.tb_sdn_int_handler      MyIngress.ac_sdn_int_handle     100     =>      2
-
-
-# This table handles the current packet using the SDN label, ac_sdn_int pop: removes the SDN label and sends the report
-# to the collector using the clone session, while reading the output port of the original packet form the Register indicated by the index
-#               Table Name                                  Action Name         SDN Label   Clone Session  Register index
-table_add   MyIngress.tb_sdn_int_handler            MyIngress.ac_sdn_int_pop        200     =>  500             1
-
-
-# tb_handle_inc matches the label of the incoming inband control packet, then it updates the relevant register from the inband control header
-#               Table Name                     Action Name                   SDN Label      Register Index
-table_add   MyIngress.tb_handle_inc     MyIngress.ac_sdn_inc_update_register    101    =>         
+      
 
 
 
