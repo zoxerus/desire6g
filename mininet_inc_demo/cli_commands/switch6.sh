61773de0877a348e4b5b9881fe02dfcd9a9a5a19
Faris <faris@null.net>
2025-10-16 09:24:30 +0200

    bugfix

--- mininet_inc_demo/cli_commands/switch6.sh
+++ mininet_inc_demo/cli_commands/switch6.sh
@@ -1,4 +1,4 @@
-table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 00:00:00:00:00:00&&&000000000000 => 2
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 00:00:00:00:00:00&&&000000000000 => 2 100
 
 
 
