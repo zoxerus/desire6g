5f167965fa5b1a213d7e561a05f247afb471dd89
Faris <faris@null.net>
2025-10-16 09:25:29 +0200

    bugfix

--- mininet_inc_demo/cli_commands/switch5.sh
+++ mininet_inc_demo/cli_commands/switch5.sh
@@ -1,10 +1,10 @@
 port_add eth0 1
 port_add eth1 4
-table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 50:7c:6f:57:6c:b0&&&FFFFFFFFFFFF => 4 100
-table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 d2:f3:57:16:2d:08&&&FFFFFFFFFFFF => 2 100
-table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 3 d2:f3:57:16:2d:08&&&FFFFFFFFFFFF => 4 100
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 50:7c:6f:57:6c:b0&&&0xFFFFFFFFFFFF => 4 100
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 d2:f3:57:16:2d:08&&&0xFFFFFFFFFFFF => 2 100
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 3 d2:f3:57:16:2d:08&&&0xFFFFFFFFFFFF => 4 100
 
-table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 4 00:00:00:00:00:00&&&000000000000 => 1 100
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 4 00:00:00:00:00:00&&&0x000000000000 => 1 100
 
 
 
--- mininet_inc_demo/cli_commands/switch6.sh
+++ mininet_inc_demo/cli_commands/switch6.sh
@@ -1,4 +1,4 @@
-table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 00:00:00:00:00:00&&&000000000000 => 2 100
+table_add  MyIngress.tb_forward_packets MyIngress.ac_set_output_port 1 00:00:00:00:00:00&&&0x000000000000 => 2 100
 
 
 
